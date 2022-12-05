14117d8c8a29e1552ef71296631f9f7be097dca2
OriginalAnswer <akiseonair@gmail.com>
2022-12-05 15:40:34 +0900

    .

--- js/mdwDate.js
+++ js/mdwDate.js
@@ -1,8 +1,8 @@
-function mdw(){
+function mdwBtn(){
     const date = new Date();
     const d = String(date.getDate());
     const m = String(date.getMonth()+1);
     bd.innerText = `${d}일`;
     bm.innerText = `${m}월`;
 }
-mdw();
+mdwBtn();
--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,13 @@
 bd.addEventListener('click', function(){
     const slider = document.getElementById('slider');
     slider.style.transform = 'translateX(0)';
-    bd.style.backgroundColor = '  '
+    // mdw.style.backgroundColor = 'gray';
 })
 bw.addEventListener('click', function(){
     const slider = document.getElementById('slider');
-    slider.style.transform = 'translateX(-100%)'
+    slider.style.transform = 'translateX(-100%)';
 })
 bm.addEventListener('click', function(){
     const slider = document.getElementById('slider');
-    slider.style.transform = 'translateX(+100%)'
+    slider.style.transform = 'translateX(+100%)';
 })
