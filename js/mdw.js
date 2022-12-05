086c89fcff390333f2172010fc3c4b1591c518e3
OriginalAnswer <akiseonair@gmail.com>
2022-12-05 14:17:22 +0900

    .

--- js/mdw.js
+++ js/mdw.js
@@ -1,6 +1,8 @@
-function thisd(){
+function mdw(){
     const date = new Date();
     const d = String(date.getDate());
+    const m = String(date.getMonth()+1);
     bd.innerText = `${d}일`;
+    bm.innerText = `${m}월`;
 }
-thisd();
+mdw();
