4cf7d5c754ad11d6676701dead6ac1381e6fd995
OriginalAnswer <akiseonair@gmail.com>
2022-12-05 15:49:47 +0900

    .

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,21 @@
+bm.addEventListener('click', function(){
+    const slider = document.getElementById('slider');
+    slider.style.transform = 'translateX(+100%)';
+    bm.style.backgroundColor = 'rgb(219, 219, 219)';
+    bd.style.backgroundColor = 'rgb(245,245,245)';
+    bw.style.backgroundColor = '';
+})
 bd.addEventListener('click', function(){
     const slider = document.getElementById('slider');
     slider.style.transform = 'translateX(0)';
-    // mdw.style.backgroundColor = 'gray';
+    bm.style.backgroundColor = '';
+    bd.style.backgroundColor = 'rgb(219, 219, 219)';
+    bw.style.backgroundColor = '';
 })
 bw.addEventListener('click', function(){
     const slider = document.getElementById('slider');
     slider.style.transform = 'translateX(-100%)';
-})
-bm.addEventListener('click', function(){
-    const slider = document.getElementById('slider');
-    slider.style.transform = 'translateX(+100%)';
+    bm.style.backgroundColor = '';
+    bd.style.backgroundColor = 'rgb(245,245,245)';
+    bw.style.backgroundColor = 'rgb(219, 219, 219)';
 })
